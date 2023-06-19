676f7370dd04f499e499519669c7bff1078323de
Shankar S <112791993+Shanky43@users.noreply.github.com>
2023-06-19 13:54:33 +0530

    final code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,70 +1,124 @@
 import {
-    Container, Heading, Box, Text, VStack, HStack, Center,  Image,
-    // Wrap, WrapItem, Avatar, Flex,
-    // Stack,
-    // IconButton, Button, Spacer, Drawer, GridItem, Grid,
-    // DrawerHeader, DrawerBody, DrawerOverlay,
-    // DrawerContent, DrawerCloseButton, Input, DrawerFooter, useColorMode,
-} from '@chakra-ui/react'
+    Container, Heading, Box, Text, VStack, HStack, Center, Image,
+
+} from '@chakra-ui/react';
 import TypeWriterEffect from 'react-typewriter-effect';
 import Design from './Design';
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+import resume from '../files/Shankar-S-Resume.pdf';
+import { Button as ChakraButton } from '@chakra-ui/react'; // Import the Button component with an alias
 
 const Home = () => {
-    const height = "600px"
+    const height = '600px';
+    const handleDownload = () => {
+        const link = document.createElement('a');
+        link.href = resume;
+        link.download = 'Shankar-S-Resume.pdf';
+        link.click();
+        window.open(
+            'https://drive.google.com/file/d/1-VtrxibWLoPWAmMKkBASzBZxDCYIsaQm/view?usp=drive_link',
+            '_blank'
+        );
+    };
+
     return (
         <div id="home">
-
-            <Container maxW={"100%"} h={["80%", "80%", "80%", "100%"]} bgColor={"black"} color="white">
-                <Box >   <Design height={height} /></Box>
-                <Center pt={"15%"}>
-                    <HStack justifyContent={"space-around"} w="80%" margin={"auto"}>
-                        <Box w={["100%", "100%", "70%", "70%"]} margin={"auto"}>
+            <Container
+                maxW={'100%'}
+                h={['80%', '80%', '80%', '100%']}
+                bgColor={'black'}
+                color="white"
+            >
+                <Box>
+                    <Design height={height} />
+                </Box>
+                <Center pt={'15%'}>
+                    <HStack justifyContent={'space-around'} w="80%" margin="auto">
+                        <Box w={['100%', '100%', '70%', '70%']} margin="auto">
                             <VStack>
                                 <HStack>
-                                    <Heading as={"h1"} size={"3xl"} m="10" textAlign={"center"}>HEY, I'M
-                                        <Text as={"span"} color='#e4002b' id="user-detail-name"> {"SHANKAR S " || <Skeleton />}</Text>
+                                    <Heading as="h1" size="3xl" m="10" textAlign="center">
+                                        HEY, I'M
+                                        <Text as="span" color="#e4002b" id="user-detail-name">
+                                            {' '}
+                                            {"SHANKAR S " || <Skeleton />}
+                                        </Text>
                                         <TypeWriterEffect
                                             textStyle={{
                                                 fontFamily: 'inherit',
                                                 color: '#66d1e5',
-                                                fontWeight: "bold",
+                                                fontWeight: 'bold',
                                                 fontSize: '0.3em',
-                                                textAlign: "center",
-                                                marginTop: "5%",
-
-
+                                                textAlign: 'center',
+                                                marginTop: '5%',
                                             }}
                                             startDelay={1000}
                                             cursorColor="#66d1e5"
                                             multiText={[
-                                                "A FULL STACK WEB DEVELOPER.", "FRONT END DEVELOPER.", "REACT DEVELOPER.", "MERN STACK DEVELOPER."
+                                                'A FULL STACK WEB DEVELOPER.',
+                                                'FRONT END DEVELOPER.',
+                                                'REACT DEVELOPER.',
+                                                'MERN STACK DEVELOPER.',
                                             ]}
                                             multiTextDelay={2000}
                                             typeSpeed={25}
                                             multiTextLoop
                                         />
-
                                     </Heading>
                                 </HStack>
-                                <Center> <Text w={"100%"} fontSize="2em" textAlign="center" id="user-detail-name">A Full Stack Web Developer focused on building the Frontend of Websites and Web Applications that leads to the success of the overall product</Text></Center>
+                                <Center>
+                                    {' '}
+                                    <Text
+                                        w="100%"
+                                        fontSize="2em"
+                                        textAlign="center"
+                                        id="user-detail-name"
+                                    >
+                                        A Full Stack Web Developer focused on building the Frontend
+                                        of Websites and Web Applications that leads to the success
+                                        of the overall product
+                                    </Text>
+                                </Center>
+                                <Box class="nav-link resume">
+                                    <ChakraButton
+                                        variant="ghost"
+                                        bg={"#e4002b"}
+                                        aria-label="RESUME"
+                                        _hover={{
+                                            color: 'white',
+                                            bg: '#e4002b',
+                                        }}
+                                        id="resume-button-1"
+                                        onClick={handleDownload}
+                                    >
+                                        RESUME
+                                    </ChakraButton>
+                                </Box>
                             </VStack>
                         </Box>
-                        <Box w={["100%", "100%", "25%", "30%"]}
-                            margin={"auto"}
-                            display={["none", "none", "none", "none", "block"]}
+                        <Box
+                            w={['100%', '100%', '25%', '30%']}
+                            margin="auto"
+                            display={['none', 'none', 'none', 'none', 'block']}
                         >
-                            <Box >
-                                <Image name='Shankar S' size='6xl' zIndex={100} className="home-img" src='https://avatars.githubusercontent.com/u/112791993?v=4' bg="black" rounded={"full"} />
+                            <Box>
+                                <Image
+                                    name="Shankar S"
+                                    size="6xl"
+                                    zIndex={100}
+                                    className="home-img"
+                                    src="https://avatars.githubusercontent.com/u/112791993?v=4"
+                                    bg="black"
+                                    rounded="full"
+                                />
                             </Box>
                         </Box>
-
                     </HStack>
                 </Center>
             </Container>
         </div>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,82 +1,121 @@
 import {
-  // GridItem, Container, Grid, Text, VStack,Input, DrawerFooter, Center,
-  Box, Wrap, WrapItem, Avatar, Flex, Heading,
-  HStack, Stack,
-  IconButton, Button, Spacer, Drawer,
-  DrawerHeader, DrawerBody, DrawerOverlay,
-  DrawerContent, DrawerCloseButton, useColorMode, Text,
-} from '@chakra-ui/react'
-import React, { useState } from 'react'
-// import AnimatePresence from '@chakra-ui/transition'  MoonIcon, SunIcon;
+  Box,
+  Wrap,
+  WrapItem,
+  Avatar,
+  Flex,
+  Heading,
+  HStack,
+  Stack,
+  IconButton,
+  Button,
+  Spacer,
+  Drawer,
+  DrawerHeader,
+  DrawerBody,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Text,
+} from '@chakra-ui/react';
+import React from 'react';
 import { Link } from 'react-scroll';
-import resume from "../files/Shankar-S-Resume.pdf"
-import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
-import { HiEye } from 'react-icons/hi';
-import { useDisclosure } from "@chakra-ui/react"
-import { AiOutlineDownload } from 'react-icons/ai';
-
+import resume from '../files/Shankar-S-Resume.pdf';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import { useDisclosure } from '@chakra-ui/react';
 
 const fornav = {
+  letterSpacing: '1px',
+  textAlign: 'center',
+  alignItems: 'center',
+};
 
-  letterSpacing: "1px",
-  textAlign: "center",
-  alignItems: "center"
-
-}
 const NavBar = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const btnRef = React.useRef()
-  const { colorMode, toggleColorMode } = useColorMode();
-  const [showIcon, setShowIcon] = useState(false)
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
+
+  const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = resume;
+    link.download = 'Shankar-S-Resume.pdf';
+    link.click();
+    window.open(
+      'https://drive.google.com/file/d/1-VtrxibWLoPWAmMKkBASzBZxDCYIsaQm/view?usp=drive_link',
+      '_blank'
+    );
+  };
+
   return (
-    <div id="nav-menu" >
-      <Flex maxW='100%' style={{ margin: "0%", padding: "0%" }} justifyContent="flex-end" >
+    <div id="nav-menu">
+      <Flex
+        maxW="100%"
+        style={{ margin: '0%', padding: '0%' }}
+        justifyContent="flex-end"
+      >
         <Flex
-          pos={"fixed"}
-          display={["none", "none", 'none', "flex"]}
+          pos="fixed"
+          display={['none', 'none', 'none', 'flex']}
           width="100%"
           zIndex={100}
-          // style={fornav}
-          pt={6} pb={3} bg={"black"} pl={10} style={fornav} color="white"
-          justifyContent={"space-between"}
+          pt={6}
+          pb={3}
+          bg={'black'}
+          pl={10}
+          style={fornav}
+          color="white"
+          justifyContent="space-between"
         >
           <Flex>
             <HStack>
-              <Wrap >
-                {/* <WrapItem >
-                  <Avatar name='Shankar S' className="home-img" src='https://avatars.githubusercontent.com/u/112791993?v=4' bg="yellow" />
-                </WrapItem> */}
+              <Wrap>
+
               </Wrap>
-              <Heading id="user-detail-name"><Text as={"span"} color={"#e4002b"}>S</Text>HANKAR.<Text as={"span"} color={"#e4002b"}>S</Text></Heading>
+              <Heading id="user-detail-name"><Text as="span" color={"#e4002b"}>S</Text>HANKAR <Text as="span" color={"#e4002b"}>S</Text></Heading>
             </HStack>
           </Flex>
           <Spacer />
           <Flex zIndex={1000}>
-            <Box >
+            <Box>
               <Button
-
                 variant="ghost"
                 arial-label="home"
                 _hover={{
-                  color: "white",
-                  bg: "#e4002b"
+                  color: 'white',
+                  bg: '#e4002b',
                 }}
               >
-                <Link to="home" className="nav-link home" spy={true} smooth={true} offset={50} duration={500}>HOME</Link>
+                <Link
+                  to="home"
+                  className="nav-link home"
+                  spy={true}
+                  smooth={true}
+                  offset={50}
+                  duration={500}
+                >
+                  HOME
+                </Link>
               </Button>
             </Box>
-
 
             <Box>
               <Button
                 variant="ghost"
                 arial-label="ABOUT"
                 _hover={{
-                  color: "white",
-                  bg: "#e4002b"
+                  color: 'white',
+                  bg: '#e4002b',
                 }}
               >
-                <Link to="about" className="nav-link about" spy={true} smooth={true} offset={50} duration={500}>ABOUT</Link>
+                <Link
+                  to="about"
+                  className="nav-link about"
+                  spy={true}
+                  smooth={true}
+                  offset={50}
+                  duration={500}
+                >
+                  ABOUT
+                </Link>
               </Button>
             </Box>
             <Box>
@@ -84,11 +123,20 @@ const NavBar = () => {
                 variant="ghost"
                 arial-label="SKILLS"
                 _hover={{
-                  color: "white",
-                  bg: "#e4002b"
+                  color: 'white',
+                  bg: '#e4002b',
                 }}
               >
-                <Link to="skills" className="nav-link skills" spy={true} smooth={true} offset={50} duration={500}>SKILLS</Link>
+                <Link
+                  to="skills"
+                  className="nav-link skills"
+                  spy={true}
+                  smooth={true}
+                  offset={50}
+                  duration={500}
+                >
+                  SKILLS
+                </Link>
               </Button>
             </Box>
             <Box>
@@ -96,11 +144,20 @@ const NavBar = () => {
                 variant="ghost"
                 arial-label="PROJECTS"
                 _hover={{
-                  color: "white",
-                  bg: "#e4002b"
+                  color: 'white',
+                  bg: '#e4002b',
                 }}
               >
-                <Link to="projects" className="nav-link projects" spy={true} smooth={true} offset={50} duration={500}>PROJECTS</Link>
+                <Link
+                  to="projects"
+                  className="nav-link projects"
+                  spy={true}
+                  smooth={true}
+                  offset={50}
+                  duration={500}
+                >
+                  PROJECTS
+                </Link>
               </Button>
             </Box>
             <Box>
@@ -108,124 +165,97 @@ const NavBar = () => {
                 variant="ghost"
                 arial-label="CONTACT"
                 _hover={{
-                  color: "white",
-                  bg: "#e4002b"
+                  color: 'white',
+                  bg: '#e4002b',
                 }}
               >
-                <Link to="contact" className="nav-link contact" spy={true} smooth={true} offset={50} duration={500}>CONTACT</Link>
+                <Link
+                  to="contact"
+                  className="nav-link contact"
+                  spy={true}
+                  smooth={true}
+                  offset={50}
+                  duration={500}
+                >
+                  CONTACT
+                </Link>
               </Button>
             </Box>
             <Box class="nav-link resume">
-              {showIcon === false ?
-                <Button
-                  variant="ghost"
-                  arial-label="RESUME"
-                  _hover={{
-                    color: "white",
-                    bg: "#e4002b"
-                  }}
-                  // id="resume-button-1"
-                  onClick={() => setShowIcon(!showIcon)}
-                >
-                  RESUME
-                </Button> : <Button
-                  variant="ghost"
-                  arial-label="RESUME"
-                  _hover={{
-                    color: "white",
-                    bg: "#e4002b"
-                  }}
-                  bg="#e4002b"
-                  id="resume-button-1"
-                  onClick={() => setShowIcon(!showIcon)}
-                  w="100px"
-                >
-                  <Box display={"flex"} justifyContent={"space-between"} width={"100%"}>
-                    <Box> <a href="https://drive.google.com/file/d/1-VtrxibWLoPWAmMKkBASzBZxDCYIsaQm/view?usp=sharing" target='_blank'><HiEye size={24} /></a></Box>
-                    <Box><a href={resume} id="resume-link-1" download><AiOutlineDownload size={24} /></a></Box>
-
-                  </Box>
-
-                  {/*  */}
-
-                </Button>
-              }
+              <Button
+                variant="ghost"
+                arial-label="RESUME"
+                _hover={{
+                  color: 'white',
+                  bg: '#e4002b',
+                }}
+                id="resume-button-1"
+                onClick={handleDownload}
+              >
+                RESUME
+              </Button>
             </Box>
           </Flex>
-          <Stack direction={'row'} spacing={7} placement="right">
-            <Button onClick={toggleColorMode} bg="black" _hover={{ bg: "black" }}>
-              {/* {colorMode === 'light' ? <MoonIcon /> : <SunIcon />} */}
-            </Button>
-          </Stack>
         </Flex>
       </Flex>
 
-
-      {/* //---------------------- */}
-
-      <Box
-        display="flex"
-        justifyContent="flex-end"
-        bgColor="black"
-      // overflow={"overlay"}
-      >
+      <Box display="flex" justifyContent="flex-end" bgColor="black">
         <Box ml="auto">
           <IconButton
-            aria-label='Open Menu'
-            size={"lg"}
+            aria-label="Open Menu"
+            size="lg"
             mr={2}
             icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            display={["flex", "flex", "flex", "none"]}
+            display={['flex', 'flex', 'flex', 'none']}
             onClick={onOpen}
             bg="black"
-            _hover={colorMode === "light" ? {
-              bg: "white",
-              color: "black"
-            } : {
-              bg: "black",
-              color: "white"
+            _hover={{
+              bg: 'black',
+              color: 'white',
             }}
-            color={colorMode === "light" ? "white" : "white"}
+            color="white"
             zIndex={20}
           />
         </Box>
       </Box>
       <Flex>
-
-
-        <Flex display={["flex", "flex", "none", "none"]} >
+        <Flex display={['flex', 'flex', 'none', 'none']}>
           <Drawer
             isOpen={isOpen}
-            placement='roght'
+            placement="roght"
             onClose={onClose}
             finalFocusRef={btnRef}
-            display={["flex", "flex", "none", "none"]}
-            overflow={"overlay"}
+            display={['flex', 'flex', 'none', 'none']}
+            overflow="overlay"
           >
             <DrawerOverlay />
             <DrawerContent>
               <DrawerCloseButton />
-              <DrawerHeader borderBottomWidth="1px">  <Flex>
-                <HStack>
-                  <Wrap>
-                    <WrapItem>
-                      <Avatar name='Shankar S' src='https://avatars.githubusercontent.com/u/112791993?v=4' bg="yellow" />
-                    </WrapItem>
-                  </Wrap>
-                  <Heading>SHANKAR S</Heading>
-                </HStack>
-              </Flex >
-
+              <DrawerHeader borderBottomWidth="1px">
+                <Flex>
+                  <HStack>
+                    <Wrap>
+                      <WrapItem>
+                        <Avatar
+                          name="Shankar S"
+                          src="https://avatars.githubusercontent.com/u/112791993?v=4"
+                          bg="yellow"
+                        />
+                      </WrapItem>
+                    </Wrap>
+                    <Heading>SHANKAR S</Heading>
+                  </HStack>
+                </Flex>
               </DrawerHeader>
 
-              <DrawerBody  >
-                <Flex >
-
-                  <Flex flexDir={"column"}
+              <DrawerBody>
+                <Flex>
+                  <Flex
+                    flexDir="column"
                     className="nav-link home"
-                    w={"100px"}
+                    w="100px"
                     align="right"
-                    display={["flex", "flex", "flex", "none"]}
+                    display={['flex', 'flex', 'flex', 'none']}
                     height="30vh"
                   >
                     <Box>
@@ -233,25 +263,24 @@ const NavBar = () => {
                         variant="ghost"
                         arial-label="home"
                         _hover={{
-                          color: "black",
-                          bg: "white"
+                          color: 'black',
+                          bg: 'white',
                         }}
                       >
-                        <Link to="home" >HOME</Link>
+                        <Link to="home">HOME</Link>
                       </Button>
                     </Box>
-
 
                     <Box>
                       <Button
                         variant="ghost"
                         arial-label="ABOUT"
                         _hover={{
-                          color: "black",
-                          bg: "white"
+                          color: 'black',
+                          bg: 'white',
                         }}
                       >
-                        <Link to="about" >ABOUT</Link>
+                        <Link to="about">ABOUT</Link>
                       </Button>
                     </Box>
                     <Box>
@@ -259,11 +288,11 @@ const NavBar = () => {
                         variant="ghost"
                         arial-label="SKILLS"
                         _hover={{
-                          color: "black",
-                          bg: "white"
+                          color: 'black',
+                          bg: 'white',
                         }}
                       >
-                        <Link to="skills" >SKILLS</Link>
+                        <Link to="skills">SKILLS</Link>
                       </Button>
                     </Box>
                     <Box>
@@ -271,11 +300,11 @@ const NavBar = () => {
                         variant="ghost"
                         arial-label="PROJECTS"
                         _hover={{
-                          color: "black",
-                          bg: "white"
+                          color: 'black',
+                          bg: 'white',
                         }}
                       >
-                        <Link to="projects" >PROJECTS</Link>
+                        <Link to="projects">PROJECTS</Link>
                       </Button>
                     </Box>
                     <Box>
@@ -283,24 +312,25 @@ const NavBar = () => {
                         variant="ghost"
                         arial-label="CONTACT"
                         _hover={{
-                          color: "black",
-                          bg: "white"
+                          color: 'black',
+                          bg: 'white',
                         }}
                       >
                         <Link to="contact">CONTACT</Link>
                       </Button>
                     </Box>
-                    <Box >
+                    <Box>
                       <Button
                         variant="ghost"
                         arial-label="RESUME"
                         _hover={{
-                          color: "black",
-                          bg: "white"
+                          color: 'black',
+                          bg: 'white',
                         }}
                         id="resume-button-2"
+                        onClick={handleDownload}
                       >
-                        <a href={resume} id="resume-link-2" download>RESUME</a>
+                        RESUME
                       </Button>
                     </Box>
                   </Flex>
@@ -309,14 +339,9 @@ const NavBar = () => {
             </DrawerContent>
           </Drawer>
         </Flex>
-
       </Flex>
-
-
-
     </div>
-
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
